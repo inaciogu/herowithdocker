e23ab040391f33e6671d159fbb52cb799b63521b
Gustavo <gus.inacio@hotmail.com>
2022-02-10 16:11:54 -0300

    change content

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('executando na porta', port));
+app.get('/', (req, res) => res.status(200).send('oi'));
 
 app.listen(port, () => console.log(`ouvindo na ${port}`))
